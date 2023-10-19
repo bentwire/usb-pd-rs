10556f694a1037caa1d41f3274b3284807fca220
Mike Panetta <panetta.mike@gmail.com>
2023-10-19 18:00:22 -0400

    Update callback.rs

--- usb-pd/src/callback.rs
+++ usb-pd/src/callback.rs
@@ -1,8 +1,7 @@
 use {crate::pdo::PowerDataObject, defmt::Format, heapless::Vec};
 
 /// Type of the user's callback function
-pub type CallbackFn = &'static dyn Fn(Event) -> Option<Response>;
-pub type SendCallbackFn = Send + CallbackFn;
+pub type CallbackFn = Send + &'static dyn Fn(Event) -> Option<Response>;
 
 /// Callback event types
 #[derive(Format)]
